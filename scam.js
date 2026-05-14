975668c508819a584dd544f0ac9969a2d1924b68
muin-15 <muinmomin2@gmail.com>
2026-05-14 15:35:51 +0530

    website url checker added

--- scam.js
+++ scam.js
@@ -1,5 +1,19 @@
 const btn=document.getElementById('btn');
+const httpsearch="http://";
+const httpssearch="https://";
+
 btn.addEventListener('click',function(){
-    alert('Congratulations! You have won a free iPhone! Click OK to claim your prize.');
-}
-);
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var Tab = tabs[0].url;
+
+    if(Tab.includes(httpsearch)){
+        alert("This website is not secure");
+    }
+    else if(Tab.includes(httpssearch)){
+        alert("This website is secure");
+    }
+    else{
+        alert("This website is not secure");
+    }
+    });
+});
